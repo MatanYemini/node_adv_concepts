3d17ff46b51aac9b686121a9a33fd7d5c59395b6
Yemini <yeminimatan@gmail.com>
2020-04-18 12:04:18 +0300

    Adding Caching, Tests, CI using Travis

--- node_proj/services/cache.js
+++ node_proj/services/cache.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const keys = require('../config/keys');
+const client = redis.createClient(keys.redisUrl);
 
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec; // holds ref to the function in mongoose
---
+++ node_proj/tests/factories/userFactory.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+
+module.exports = () => {
+    
+    return new User({}).save();
+}
